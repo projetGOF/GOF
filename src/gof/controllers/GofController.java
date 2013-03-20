a9bdb9bc1de6d82bf3641b3c99bf9a700caea5c7
projetGOF <mirallesjerome@hotmail.fr>
2013-03-20 16:04:33 +0100

    Ajout de Jspx pour parcourir l'arbre.

--- src/gof/controllers/GofController.java
+++ src/gof/controllers/GofController.java
@@ -23,13 +23,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class GofController
 {
 	@Autowired
-	PersonneManager personneManager;
-	
-	@Autowired
 	MentionManager mentionManager;
 	
 	@Autowired
 	DomaineManager domaineManager;
+	
+	@Autowired
+	SpecialiteManager specialiteManager;
+	
+	@Autowired
+	ProgrammeManager programmeManager;
 	
 	@RequestMapping("/accueil.htm")
 	public ModelAndView home(Model model)
@@ -60,83 +63,106 @@ public class GofController
 	@RequestMapping("/licence{diplome}.htm")
 	public ModelAndView domaines(@PathVariable String diplome)
 	{
-		System.out.println(diplome);
 		TypeMention type = TypeMention.LICENCE;
 		
-		Map<String, Object> domaineModel = new HashMap<String, Object>();
+		Map<String, Object> model = new HashMap<String, Object>();
 		
-		domaineModel.put("type", diplome);
-		domaineModel.put("domaines", this.mentionManager.findAllDomaineByTypeMention(type));
-		return new ModelAndView("licence", "model", domaineModel);
+		model.put("type", diplome);
+		model.put("domaines", this.mentionManager.findAllDomaineByTypeMention(type));
+		return new ModelAndView("licence", "model", model);
 	}
 	
 	@RequestMapping("/licenceDetail{domaine}.htm")
 	public ModelAndView domainesDetail(@PathVariable String domaine)
 	{
-		System.out.println(domaine);
 		TypeMention type = TypeMention.LICENCE;
-        Map<String, Object> domaineDetailModel = new HashMap<String, Object>();
-        domaineDetailModel.put("domaine", this.domaineManager.findDomaine(domaine));
-        domaineDetailModel.put("mentions", this.mentionManager.findAllMentionsByDomaineAndTypeMention(domaine, type));
-        return new ModelAndView("licenceDetail", "model", domaineDetailModel);
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("domaine", this.domaineManager.findDomaine(domaine));
+        model.put("mentions", this.mentionManager.findAllMentionsByDomaineAndTypeMention(domaine, type));
+        return new ModelAndView("licenceDetail", "model", model);
 	}
 	
 	@RequestMapping("/licencePro{diplome}.htm")
 	public ModelAndView licencePro(@PathVariable String diplome)
 	{
-		System.out.println(diplome);
 		TypeMention type = TypeMention.LICENCEPRO;
 		
-		Map<String, Object> domaineModel = new HashMap<String, Object>();
+		Map<String, Object> model = new HashMap<String, Object>();
 		
-		domaineModel.put("type", diplome);
-		domaineModel.put("domaines", this.mentionManager.findAllDomaineByTypeMention(type));
-		return new ModelAndView("licencePro", "model", domaineModel);
+		model.put("type", diplome);
+		model.put("domaines", this.mentionManager.findAllDomaineByTypeMention(type));
+		return new ModelAndView("licencePro", "model", model);
 	}
 	
 	@RequestMapping("/licenceProDetail{domaine}.htm")
 	public ModelAndView licenceProDetail(@PathVariable String domaine)
 	{
-		System.out.println(domaine);
 		TypeMention type = TypeMention.LICENCEPRO;
-        Map<String, Object> domaineDetailModel = new HashMap<String, Object>();
-        domaineDetailModel.put("domaine", this.domaineManager.findDomaine(domaine));
-        domaineDetailModel.put("mentions", this.mentionManager.findAllMentionsByDomaineAndTypeMention(domaine, type));
-        return new ModelAndView("licenceProDetail", "model", domaineDetailModel);
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("domaine", this.domaineManager.findDomaine(domaine));
+        model.put("mentions", this.mentionManager.findAllMentionsByDomaineAndTypeMention(domaine, type));
+        return new ModelAndView("licenceProDetail", "model", model);
 	}
 	
 	@RequestMapping("/master{diplome}.htm")
 	public ModelAndView master(@PathVariable String diplome)
 	{
-		System.out.println(diplome);
 		TypeMention type = TypeMention.MASTER;
 		
-		Map<String, Object> domaineModel = new HashMap<String, Object>();
+		Map<String, Object> model = new HashMap<String, Object>();
 		
-		domaineModel.put("type", diplome);
-		domaineModel.put("domaines", this.mentionManager.findAllDomaineByTypeMention(type));
-		return new ModelAndView("master", "model", domaineModel);
+		model.put("type", diplome);
+		model.put("domaines", this.mentionManager.findAllDomaineByTypeMention(type));
+		return new ModelAndView("master", "model", model);
 	}
 	
 	@RequestMapping("/masterDetail{domaine}.htm")
 	public ModelAndView masterDetail(@PathVariable String domaine)
 	{
-		System.out.println(domaine);
 		TypeMention type = TypeMention.MASTER;
-        Map<String, Object> domaineDetailModel = new HashMap<String, Object>();
-        domaineDetailModel.put("domaine", this.domaineManager.findDomaine(domaine));
-        domaineDetailModel.put("mentions", this.mentionManager.findAllMentionsByDomaineAndTypeMention(domaine, type));
-        return new ModelAndView("masterDetail", "model", domaineDetailModel);
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("domaine", this.domaineManager.findDomaine(domaine));
+        model.put("mentions", this.mentionManager.findAllMentionsByDomaineAndTypeMention(domaine, type));
+        return new ModelAndView("masterDetail", "model", model);
 	}
 	
 	@RequestMapping("/mention{mention}.htm")
 	public ModelAndView mention(@PathVariable String mention)
 	{
-		System.out.println(mention);
-
-        Map<String, Object> mentionDetailModel = new HashMap<String, Object>();
-        mentionDetailModel.put("mention", this.mentionManager.findMention(mention));
-        return new ModelAndView("mention", "model", mentionDetailModel);
+		Map<String, Object> model = new HashMap<String, Object>();
+		Mention ment = this.mentionManager.findMention(mention);
+		
+		Set<Specialite> spe = ment.getSpecialites();
+		Hibernate.initialize(spe);
+		
+		model.put("mention", ment);
+		model.put("specialites", spe);
+        return new ModelAndView("mention", "model", model);
+	}
+	
+	@RequestMapping("/specialite{specialite}.htm")
+	public ModelAndView specialite(@PathVariable String specialite)
+	{
+		Map<String, Object> model = new HashMap<String, Object>();
+        Specialite spe = this.specialiteManager.findSpecialite(specialite);
+        
+        Set<Programme> prog = spe.getProgrammes();
+        Hibernate.initialize(prog);
+        
+        model.put("specialite", spe);
+        model.put("programmes", prog);
+        return new ModelAndView("specialite", "model", model);
+	}
+	
+	@RequestMapping("/programme{programme}.htm")
+	public ModelAndView programme(@PathVariable String programme)
+	{
+		Map<String, Object> model = new HashMap<String, Object>();
+        Programme prog = this.programmeManager.findProgramme(programme);
+        
+        
+        model.put("programme", prog);
+        return new ModelAndView("programme", "model", model);
 	}
 	
 	@RequestMapping("/etat.htm")
